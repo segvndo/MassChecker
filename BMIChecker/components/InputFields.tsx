9e71979cc3640d9769bfa683a2376dfc5e736498
Bea Mungcal <beamungcal@hotmail.com>
2024-02-09 06:17:02 -0500

    Add conditions, errors, and notifications to successful submission

--- BMIChecker/components/InputFields.tsx
+++ BMIChecker/components/InputFields.tsx
@@ -7,19 +7,45 @@ const InputFields: React.FC = () => {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [age, setAge] = useState('');
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [bmi, setBMI] = useState<number | null>(null);
 
-  const handleNumericChange = (inputText: string, setter: React.Dispatch<React.SetStateAction<string>>) => {
+  const handleNumericChange = (inputText: string, setter: React.Dispatch<React.SetStateAction<string>>, field: string) => {
     const numericInput = inputText.replace(/[^0-9]/g, '');
     const limitedInput = numericInput.slice(0, 3);
     setter(limitedInput);
+
+    // Validate input
+    if (!limitedInput) {
+      setErrors(prevErrors => ({ ...prevErrors, [field]: '*' }));
+    } else {
+      setErrors(prevErrors => ({ ...prevErrors, [field]: '' }));
+    }
   };
 
   const calculateBMI = () => {
+    // Validate all input fields before proceeding
+    if (!age || !weight || !height) {
+      setErrors({
+        age: !age ? '*' : '',
+        weight: !weight ? '*' : '',
+        height: !height ? '*' : '',
+      });
+      return;
+    }
+
+    // Calculate BMI if all fields are filled
     const weightInKg = parseFloat(weight);
     const heightInM = parseFloat(height) / 100; // Convert height from cm to m
     const bmiValue = weightInKg / (heightInM * heightInM);
+    // Reset errors
+    setErrors({});
     setBMI(bmiValue);
+
+    // Clear input fields
+    setWeight('');
+    setHeight('');
+    setAge('');
   };
 
   return (
@@ -29,21 +55,23 @@ const InputFields: React.FC = () => {
         <FontAwesome name="user" size={24} color="#008000" />
         <TextInput
           style={styles.inputField}
-          onChangeText={(text) => handleNumericChange(text, setAge)}
+          onChangeText={(text) => handleNumericChange(text, setAge, 'age')}
           value={age}
           placeholder='00'
         />
+        {errors.age && <Text style={styles.error}>{errors.age}</Text>}
       </View>
       <View style={styles.inputRow}>
         <Text style={styles.titles}>Weight(kg)</Text>
         <FontAwesome name="balance-scale" size={24} color="#008000" />
         <TextInput
           style={styles.inputField}
-          onChangeText={(text) => handleNumericChange(text, setWeight)}
+          onChangeText={(text) => handleNumericChange(text, setWeight, 'weight')}
           value={weight}
           keyboardType='numeric'
           placeholder='00kg'
         />
+        {errors.weight && <Text style={styles.error}>{errors.weight}</Text>}
       </View>
       <View style={styles.inputRow}>
         <Text style={styles.titles}>Height(cm)</Text>
@@ -52,14 +80,19 @@ const InputFields: React.FC = () => {
         </View>
         <TextInput
           style={styles.inputField}
-          onChangeText={(text) => handleNumericChange(text, setHeight)}
+          onChangeText={(text) => handleNumericChange(text, setHeight, 'height')}
           value={height}
           keyboardType='numeric'
           placeholder='00cm'
         />
+        {errors.height && <Text style={styles.error}>{errors.height}</Text>}
       </View>
-      <Button title='Calculate BMI' onPress={calculateBMI} />
-      {bmi !== null && <Text>Your BMI: {bmi.toFixed(2)}</Text>}
+      <View >
+        <Button title='Calculate BMI' onPress={calculateBMI} />
+      </View>
+      <View>
+        {bmi !== null && <Text style={styles.result}>Your BMI: {bmi.toFixed(2)}</Text>}
+      </View>
     </View>
   );
 };
@@ -71,6 +104,7 @@ const styles = StyleSheet.create({
     color: '#008000',
   },
   inputRow: {
+    position: 'relative',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -92,6 +126,27 @@ const styles = StyleSheet.create({
     marginVertical: 6,
     fontSize: 20,
     color: '#008000'
+  },
+  button: {
+    backgroundColor: '#008000',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#008000',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  error: {
+    color: 'red',
+    fontSize: 18,
+    marginTop: 5,
+    position: 'absolute',
+    top: 2,
+    right: 2,
   }
 });
 
